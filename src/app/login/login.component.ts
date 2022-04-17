fcc3cf847a8ef72ad1f33d942d949ccbab4e8068
Marcs20 <marcosj3oliveira@gmail.com>
2022-04-17 20:18:50 -0300

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from '../model/user';
 import { RegistrationService } from '../service/registration.service';
 
@@ -9,14 +10,22 @@ import { RegistrationService } from '../service/registration.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private _service: RegistrationService) {}
+  constructor(private _service: RegistrationService, private _router: Router) {}
 
   user = new User();
 
+  msg = '';
+
   loginUser() {
     this._service.loginUserFromRemote(this.user).subscribe(
-      (data) => console.log('received'),
-      (error) => console.log('exception')
+      (data) => {
+        console.log('received');
+        this._router.navigate(['/home']);
+      },
+      (error) => {
+        console.log('exception');
+        this.msg = 'bad credentials';
+      }
     );
   }
 
