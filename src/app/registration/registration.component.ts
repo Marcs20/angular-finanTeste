f610fd67c24e3a2bdeb0d7c9150aca734a7384b1
Marcs20 <marcosj3oliveira@gmail.com>
2022-04-17 21:23:27 -0300

    2 files modified

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-registration',
@@ -6,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private _service: RegistrationComponent,
+    private _router: Router
+  ) {}
+
+  user = new User();
+
+  registerUser() {}
 
   ngOnInit() {}
 }
