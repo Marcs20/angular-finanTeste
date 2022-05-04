eb2ade525133e1c9560e389bfa32ebe540a0f039
Marcs20 <marcosj3oliveira@gmail.com>
2022-05-03 22:36:56 -0300

    2 files modified

--- src/app/despesa/mostrar/mostrar.component.ts
+++ src/app/despesa/mostrar/mostrar.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Lancamento } from '../../model/lancamento';
+import { Receita } from '../../model/receita';
 
 import { FinancaService } from '../../service/financa.service';
 
@@ -11,7 +12,11 @@ import { FinancaService } from '../../service/financa.service';
   styleUrls: ['./mostrar.component.css'],
 })
 export class MostrarComponent implements OnInit {
-  constructor(public service: FinancaService, private router: Router, private http: HttpClient) {}
+  constructor(
+    public service: FinancaService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   lancamento: Lancamento[] = [];
 
@@ -26,27 +31,6 @@ export class MostrarComponent implements OnInit {
     localStorage.setItem('id', lanca.id.toString());
     this.router.navigate(['editar']);
   }
-
-  dataL: Array<Lancamento> = [];
-  dataR: Array<Lancamento> = [];
-
-  getDesp() {
-    this.http
-      .get<Lancamento>('https://aplica1.herokuapp.com/lancamento')
-      .subscribe((data) => this.dataL.push(data));
-  }
-
-  getValueL() {
-    return this.saldo = this.dataL[length].valor + this.dataR[length].valor;
-  }
-
-  getRece() {
-    this.http
-      .get<Lancamento>('https://aplica1.herokuapp.com/receita')
-      .subscribe((data) => this.dataR.push(data));
-  }
-
-  saldo: number = 0;
 
   ngOnInit() {
     this.service.getLancamento().subscribe((data) => (this.lancamento = data));
