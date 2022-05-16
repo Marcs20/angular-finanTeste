1b328d3fd619ac68de867db4c5e18bb9e92b0c36
Marcs20 <marcosj3oliveira@gmail.com>
2022-05-16 09:40:45 -0300

    2 files modified

--- src/app/calculadoras/juros-sim/juros-sim.component.ts
+++ src/app/calculadoras/juros-sim/juros-sim.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-juros-sim',
@@ -12,13 +13,27 @@ export class JurosSimComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
-
   getJurosSimp() {
     let list: number[] = [];
     for (let x = 1; x <= this.t; x++) {
       list.push(this.c * (1 + (this.i / 100) * x));
     }
     return list;
+  }
+
+  @ViewChild('myCanvas', { static: true }) elemento: ElementRef;
+  ngOnInit() {
+    new Chart(this.elemento.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            backgroundColor: ['#0E73F5', 'red', 'green', 'orange'],
+          },
+        ],
+      },
+    });
   }
 }
