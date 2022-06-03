8e9b7eaddf24c7c158e3e0744c273ebf3e783a37
Marcs20 <marcosj3oliveira@gmail.com>
2022-06-03 12:12:47 -0300

    1 file modified

--- src/app/calculadoras/rent-pou/rent-pou.component.ts
+++ src/app/calculadoras/rent-pou/rent-pou.component.ts
@@ -10,14 +10,14 @@ export class RentPouComponent implements OnInit {
 
   c = 0;
   i = 0;
-  r = 0;
+  r = 0.1733 / 100;
   t = 0;
   s = 0;
 
   getJurosComp() {
     let list: number[] = [];
     for (let x = 1; x <= this.t; x++) {
-      list.push((this.s = this.s + (this.c * this.r) / 100));
+      list.push((this.c += this.c * this.r));
     }
     return list;
   }
